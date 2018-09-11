e6ba0e5c8c59f6e03f9b8ff5f3515e09c0c9b5ef
Murali Veesambattu <MVeesambattu@simeiosolutions.com>
2018-09-11 10:01:34 +0530

    CRUD App

---
+++ data.js
@@ -0,0 +1,92 @@
+var data = [
+    {
+        "fname": "Earnest",
+        "lname": "Angell",
+        "tel": "(799)311-4375",
+        "address": "3186 Tellus St",
+        "city": "Conover",
+        "state": "SC",
+        "zip": 11015
+    },
+    {
+        "fname": "Bingmei",
+        "lname": "Kaa",
+        "tel": "(201)433-4457",
+        "address": "2367 Tellus St",
+        "city": "Rancho Cordova",
+        "state": "LA",
+        "zip": 92545
+    },
+    {
+        "fname": "Eden",
+        "lname": "Curtis",
+        "tel": "(870)714-1827",
+        "address": "4152 Ante Ave",
+        "city": "Stony Point",
+        "state": "OR",
+        "zip": 80600
+    },
+    {
+        "fname": "Earnestine",
+        "lname": "Kesner",
+        "tel": "(424)625-0894",
+        "address": "7064 At Rd",
+        "city": "Minneola",
+        "state": "NE",
+        "zip": 68775
+    },
+    {
+        "fname": "Shelley",
+        "lname": "Duerksen",
+        "tel": "(232)065-7274",
+        "address": "4703 Facilisis St",
+        "city": "Hampton Roads",
+        "state": "MD",
+        "zip": 82042
+    },
+    {
+        "fname": "Asif",
+        "lname": "Pomykala",
+        "tel": "(937)970-4359",
+        "address": "9957 Et Rd",
+        "city": "Indy",
+        "state": "DC",
+        "zip": 48443
+    },
+    {
+        "fname": "Anuj",
+        "lname": "Belisario",
+        "tel": "(594)293-5225",
+        "address": "2722 Sit Ln",
+        "city": "Concord",
+        "state": "TX",
+        "zip": 45069
+    },
+    {
+        "fname": "Wayne",
+        "lname": "Tran",
+        "tel": "(313)080-4687",
+        "address": "2273 Orci Ct",
+        "city": "Arlington",
+        "state": "ND",
+        "zip": 76826
+    },
+    {
+        "fname": "Dolores",
+        "lname": "Brewer",
+        "tel": "(837)500-4017",
+        "address": "2457 Sit Ave",
+        "city": "Catlettsburg",
+        "state": "CO",
+        "zip": 93171
+    },
+    {
+        "fname": "Marisela",
+        "lname": "Wathen",
+        "tel": "(221)003-9128",
+        "address": "3251 Tortor Rd",
+        "city": "Texas City",
+        "state": "IN",
+        "zip": 25557
+    }
+]
