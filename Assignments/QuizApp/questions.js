60d739708bbb62d6e967f9a4c7a4128139244882
vmurali100 <vmurali100@users.noreply.github.com>
2018-09-28 10:45:43 +0530

    Quiz App

---
+++ Assignments/QuizApp/questions.js
@@ -0,0 +1,9 @@
+function Question (text,choice,answer){
+	this.text=text;
+	this.choice=choice;
+	this.answer=answer;
+}
+
+Question.prototype.correctAnswer=function (choice){
+	return choice===this.answer;
+}
